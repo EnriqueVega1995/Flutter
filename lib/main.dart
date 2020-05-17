e349258f087c1ae2a6743c3566380279653a64d6
EnriqueVega1995 <vegacampos1995@gmail.com>
2020-05-16 18:16:33 -0600

    Add list and style

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,56 @@ void main(){
 class MyApp extends StatelessWidget {
  @override
  Widget build(BuildContext context) {
-   final wordPair = new WordPair.random();
    return MaterialApp(
      title: 'Welcome to Flutter',
-     home: Scaffold(
+     home: RamdomWords(),
+   );
+ }
+}
+
+class RamdomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return RamdomWordsState();
+  }
+}
+
+class RamdomWordsState extends State<RamdomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
        appBar: AppBar(
          title: Text('Welcome to Flutter'),
        ),
-       body: Center(
-         child: Text(wordPair.asPascalCase),
-       ),
-     ),
-   );
- }
+       body: _buildSuggestions(),
+     );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i){
+        if(i.isOdd){
+          return Divider();
+        }
+        if(i >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        final index = i ~/ 2;
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+        ),
+    );
+  }
 }
